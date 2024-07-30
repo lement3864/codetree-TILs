703a0c6f8493981f8704ab6d9d9ab371c88b8737
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-30 22:18:57 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num1 = 97, num2 = 13;
+    int minus = num1 - num2;
+
+    cout << num1 << " - " << num2 << " = " << minus;
+
+    return 0;
+}
