788e837afc4ef034ee468c8e3ff68db6005638d6
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-31 14:32:36 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << fixed;
+    double n;
+
+    cin >> n;
+
+    cout.precision(2);
+    cout << n;
+    
+    return 0;
+}
