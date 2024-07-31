0a1d5b0ae8405ced32d60931da3d58833d2d1b89
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-31 14:14:05 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 3, b = 4;
+    
+    b = a;
+
+    cout << a << " " << b << "\n";
+    cout << a * b;
+
+    return 0;
+}
