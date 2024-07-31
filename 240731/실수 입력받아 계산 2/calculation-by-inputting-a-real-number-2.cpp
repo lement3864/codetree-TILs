8d5f937aed5d9d9bf3aa850bf4e4ff2f5d915365
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-31 14:35:29 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << fixed;
+
+    double a;
+
+    cin >> a;
+
+    cout.precision(2);
+    cout << a + 1.5;
+
+    return 0;
+}
