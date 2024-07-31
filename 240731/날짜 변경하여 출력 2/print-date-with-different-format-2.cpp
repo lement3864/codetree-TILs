3a252e3bcbb4e7512a655ad067a69c40c93afd5c
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-31 18:15:13 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int mm, dd, yyyy;
+
+    cin >> mm;
+    cin.get();
+    cin >> dd;
+    cin.get();
+    cin >> yyyy;
+
+    cout << yyyy << "." << mm << "." << dd;
+
+    return 0;
+}
