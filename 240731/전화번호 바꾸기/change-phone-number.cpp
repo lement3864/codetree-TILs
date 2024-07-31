b0ddf2ff48d95032e48a5b87fac4c55aa77a2a53
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-07-31 18:26:05 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int num, xxxx, yyyy;
+
+    char d;
+
+    cin >> num >> d >> xxxx >> d >> yyyy;
+
+    cout << "010" << d << yyyy << d << xxxx;
+
+    return 0;
+}
