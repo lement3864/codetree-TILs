ec9201bf9b7001b340733f1bb98f90f874b653cd
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-01 15:28:41 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout.precision(2);
+    cout << (double)(a + b) / (a - b);
+
+    return 0;
+}
