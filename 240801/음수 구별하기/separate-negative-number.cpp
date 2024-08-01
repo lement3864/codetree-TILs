4a973b4a0c6cae12b4701b60fb7774a8d11825e0
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-01 15:41:49 +0900

    Title: 음수 구별하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+
+    cin >> num;
+
+    if (num < 0) 
+    {
+        cout << num << "\n" << "minus";
+    }
+
+    return 0;
+}
