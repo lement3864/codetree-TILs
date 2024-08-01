495d68fa847f767cc021366fa4627e278d43070f
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-01 15:49:09 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    cout << n * n << "\n";
+    
+    if (n < 5)
+    {
+        cout << "tiny";
+    }
+
+    return 0;
+}
