5a848a39d5caa29c352219bc9b14ef151f21ba73
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-02 21:00:22 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+
+    cin >> num;
+
+    if (num == 1)
+    {
+        cout << "John";
+    }
+    else if (num == 2)
+    {
+        cout << "Tom";
+    }
+    else
+    {
+        cout << "Paul";
+    }
+
+    return 0;
+}
