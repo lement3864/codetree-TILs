33de94398bde98e05accc4083cc6da5d5d057cd6
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-02 22:09:41 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240802/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+
+    cin >> a >> b;
+
+    if (a < b)
+    {
+        cout << 1 << " ";
+    }
+    else
+    {
+        cout << 0 << " ";
+    }
+    
+    if (a == b)
+    {
+        cout << 1 << " ";
+    }
+    else
+    {
+        cout << 0 << " ";
+    }
+
+    return 0;
+}
