bcc728cf9362279b5a44634ecc5b509d54e4c677
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-02 22:46:14 +0900

    Title: 세 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240802/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240802/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -7,15 +7,15 @@ int main() {
 
     cin >> a >> b >> c;
 
-    if (a < b && a < c)
+    if (a <= b && a <= c)
     {
         cout << a;
     }
-    else if (b < a && b < c)
+    else if (b <= a && b <= c)
     {
         cout << b;
     }
-    else if (c < a && c < b)
+    else if (c <= a && c <= b)
     {
         cout << c;
     }
