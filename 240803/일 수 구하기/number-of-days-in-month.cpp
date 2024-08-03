43ac58a8b9c1eed424254e21dfc3a2d0984d6dad
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-03 12:44:21 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    if (n == 2)
+    {
+        cout << 28;
+    }
+    else
+    {
+        if (n % 2 == 0)
+        {
+            cout << 30;
+        }
+        else
+        {
+            cout << 31;
+        }
+    }
+
+    return 0;
+}
