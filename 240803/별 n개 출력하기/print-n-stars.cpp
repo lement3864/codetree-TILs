34ee94c8d9c6efacc73886f0b564f696658887ed
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-03 14:15:29 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n, i = 0;
+
+    cin >> n;
+
+    while (i < n)
+    {
+        cout << "*" << endl;
+
+        ++i;
+    }
+
+    return 0;
+}
