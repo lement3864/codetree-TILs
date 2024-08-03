811098f663711f57d9e8bcb71ca23c363d2521b4
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-03 12:41:42 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/윤년인가/is-leap-year.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int y;
+
+    cin >> y;
+
+    if (y % 4 != 0 || (y % 100 == 0 && y % 400 != 0))
+    {
+        cout << "false";
+    }
+    else
+    {
+        cout << "true";
+    }
+
+    return 0;
+}
