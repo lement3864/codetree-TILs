bb46a78794c20a93209c6d358f43ead6870ced36
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-04 12:12:40 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    while (n >= 1)
+    {
+        cout << n << " ";
+
+        n--;
+    }
+
+    return 0;
+}
