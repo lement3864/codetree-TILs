a33de3f54714dc7eba5f5c64169dabdc90d5e060
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-04 13:18:19 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/문자 출력하기/print-text.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    char text;
+
+    cin >> text;
+
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << text;
+    }
+
+    return 0;
+}
