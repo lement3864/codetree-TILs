b0b8d1857035487da10e7d63ac92c95abf3ae12a
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-04 14:16:54 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240804/369 게임/369-game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        if (i == 3 || i == 6 || i == 9)
+        {
+            cout << 0 << " ";
+        }
+        else
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
