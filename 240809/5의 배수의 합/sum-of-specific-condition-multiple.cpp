bef1db598f8481afc3ef88849b35aee20e427c17
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-09 22:14:54 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b, sum_val = 0;
+
+    cin >> a >> b;
+
+    if (a >= b)
+    {
+        for (int i = b; i <= a; ++i)
+        {
+            if (i % 5 == 0)
+            {
+                sum_val += i;
+            }
+        }
+    }
+    else
+    {
+        for (int i = a; i <= b; ++i)
+        {
+            if (i % 5 == 0)
+            {
+                sum_val += i;
+            }
+        }
+    }
+
+    cout << sum_val;
+
+    return 0;
+}
