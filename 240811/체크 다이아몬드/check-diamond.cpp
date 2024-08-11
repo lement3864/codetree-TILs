fda01fdeff3080e0cd63a71945f02e31b39726dd
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-11 13:41:12 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int v = 0; v < n - i - 1; ++v)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; ++j)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int v = 0; v <= i; ++v)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
