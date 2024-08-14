8e0a964e12d6daa7336d5f3f9fd09b8f863ed665
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-14 22:27:23 +0900

    Title: 모양대로 숫자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240814/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+
+    cin >> n;
+
+    for (int i = n; i > 0; --i)
+    {
+        for (int v = 0; v < n - i; ++v)
+        {
+            cout << "  ";
+        }
+
+        for (int j = i; j > 0; --j)
+        {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
