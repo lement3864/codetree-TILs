ee9530e854e2cb8c6dc4fca49061346d5729e31b
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-14 21:44:33 +0900

    Title: 구구단 만들기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240814/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+
+    cin >> a >> b;
+
+    for (int i = 1; i <= 9; ++i)
+    {
+        for (int j = b; j >= a; --j)
+        {
+            if (j % 2 == 0)
+            {
+                cout << j << " * " << i << " = " << j * i;
+
+                if (j != 2)
+                    cout << " / ";
+            }
+        }
+
+        cout << endl;
+    }
+    
+    return 0;
+}
