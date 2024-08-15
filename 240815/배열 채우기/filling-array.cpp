e57c848b664db24ab850ab9ea156ca3c9633944a
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-15 15:12:01 +0900

    Title: 배열 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/배열 채우기/filling-array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int cnt = 0, arr[10];
+
+    for (int i = 0; i < 10; ++i)
+    {
+        cin >> arr[i];
+
+        cnt++;
+
+        if (arr[i] == 0)
+            break;
+    }
+
+    for (int i = cnt - 2; i >= 0; --i)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
