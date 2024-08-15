37c5d6d5944c0fcf3b4886c8f62a4f0474ad90ed
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-15 11:44:42 +0900

    Title: 알파벳 출력하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240815/알파벳 출력하기/print-alphabet.cpp
+++ 240815/알파벳 출력하기/print-alphabet.cpp
@@ -11,7 +11,7 @@ int main() {
     {
         for (int j = 1; j <= i; ++j)
         {
-            if (cnt == 88)
+            if ((char)cnt == 'Z')
                 cnt = 65;
 
             cout << (char)cnt;
