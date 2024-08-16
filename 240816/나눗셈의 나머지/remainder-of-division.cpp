6c557fdc7a6e1793a6d884ac9f33e4f6efe14cc6
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-16 21:06:03 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240816/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b, temp, sum_val = 0; 
+    int remain[11] = {};
+
+    cin >> a >> b;
+
+    while (a >= 1)
+    {
+        temp = a / b;
+ 
+        remain[a % b]++;
+        
+        a = temp;
+    }
+
+    for (int i = 0; i < 11; ++i)
+    {
+        sum_val += remain[i] * remain[i];
+    }
+
+    cout << sum_val;
+
+    return 0;
+}
