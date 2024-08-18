dded2cc1756840bd3aaa609b417c12be5130c6ac
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-18 11:59:29 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int len_A, len_B;
+
+    cin >> len_A >> len_B;
+
+    int arr_A[len_A];
+    int arr_B[len_B];
+
+    for (int i = 0; i < len_A; ++i)
+    {
+        cin >> arr_A[i];
+    }
+    for (int i = 0; i < len_B; ++i)
+    {
+        cin >> arr_B[i];
+    }
+
+    int cnt = 0;
+
+    for (int i = 0; i < len_A; ++i)
+    {
+
+        for (int j = 0; j < len_B; ++j)
+        {
+            if (arr_A[i + j] == arr_B[j])
+            {
+                cnt++;
+                continue;
+            }
+            else
+            {
+                cnt = 0;
+                break;
+            }
+        }
+    }
+
+    if (cnt == len_B)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
