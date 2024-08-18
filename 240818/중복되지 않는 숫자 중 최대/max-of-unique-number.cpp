62056500ee3084c983dcc56e10ab693550b9c69c
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-18 14:07:30 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    
+    int n;
+    int max_val = INT_MIN, check = INT_MAX;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    for (int t = 0; t < n; ++t)
+    {
+        bool count = false;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (arr[i] == max_val)
+            {
+                check = arr[i];
+                max_val = INT_MIN;
+                count = true;
+                break;
+            }
+
+            if (arr[i] < check && arr[i] >= max_val)
+            {
+                max_val = arr[i];
+            }
+        }
+
+        if (max_val <= check && count == false)
+        {
+            cout << max_val;
+            break;
+        }
+    }
+
+    return 0;
+}
