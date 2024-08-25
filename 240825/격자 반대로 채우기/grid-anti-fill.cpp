2c54d799b069623ece2f5f5c034b41941b61cffa
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 11:04:28 +0900

    Title: 격자 반대로 채우기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240825/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n, num = 1;
+    int arr[10][10] = {};
+
+    cin >> n;
+
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (i % 2 != 0)
+        {
+            for (int j = n - 1; j >= 0; --j)
+            {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+        else
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                arr[j][i] = num;
+                num++;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
