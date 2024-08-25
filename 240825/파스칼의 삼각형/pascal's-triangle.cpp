46d0fa23737849c23eb67173a41d13d98979483e
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 11:46:10 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240825/파스칼의 삼각형/pascal's-triangle.cpp
+++ 240825/파스칼의 삼각형/pascal's-triangle.cpp
@@ -12,10 +12,12 @@ int main() {
     {
         for (int j = 0; j <= i; ++j)
         {
+            // 배열의 외각 1로 채우기
             if (j == 0 || j == i)
             {
                 arr[i][j] = 1;
             }
+            // 위쪽 행의 두 값의 합을 채우기
             else
             {
                 arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
@@ -23,6 +25,7 @@ int main() {
         }
     }
 
+    // 출력
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j <= i; ++j)
