38d352bdc22f729a4942713846e28063eba199d0
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 21:47:06 +0900

    Title: 문자열 범위 출력하기 2, Time: 18ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240825/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240825/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -19,7 +19,7 @@ int main() {
     }
     else
     {
-        for (int i = str.length() - 1; i > 0; --i)
+        for (int i = str.length() - 1; i >= 0; --i)
         {
             cout << str[i];
         }
