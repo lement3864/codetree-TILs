0f4524ca72bc665d39b0c633f28fe1c00aefd0c1
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 13:58:21 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240825/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string str[10];
+
+    for (int i = 0; i < 10; ++i)
+    {
+        cin >> str[i];
+
+        if (i % 2 == 0)
+        {
+            cout << str[i] << endl;
+        }
+    }
+
+    return 0;
+}
