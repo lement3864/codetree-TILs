d7e109506976fc675121199b33588dd214edb186
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 11:28:48 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240825/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[5][5] = {};
+
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            // 0번째 행과 열은 모두 1로 초기화
+            if (i == 0 || j == 0)
+            {
+                arr[i][j] = 1;
+            }
+            
+            if (i > 0 && j > 0)
+            {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+    }
+
+    // 출력
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = 0; j < 5; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
