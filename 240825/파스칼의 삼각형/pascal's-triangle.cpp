5103127261aea4b6d899db5de7e6b81e6c938857
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-08-25 11:44:55 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240825/파스칼의 삼각형/pascal's-triangle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    int arr[15][15] = {};
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            if (j == 0 || j == i)
+            {
+                arr[i][j] = 1;
+            }
+            else
+            {
+                arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
