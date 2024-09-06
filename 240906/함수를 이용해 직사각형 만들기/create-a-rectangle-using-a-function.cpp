b7748c19f857cf99fd39cbfa238aeea7a7d09138
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-06 18:40:38 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240906/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void Print1(int n, int m) {
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            cout << 1;
+        }
+        cout << endl;
+    }
+
+}
+
+int main() {
+    
+    int n, m;
+
+    cin >> n >> m;
+
+    Print1(n, m);
+
+    return 0;
+}
