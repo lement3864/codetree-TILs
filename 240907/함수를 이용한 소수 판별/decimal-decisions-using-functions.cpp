482a2072259bde5e7444e853a9b32f0a0a1abeb5
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-07 09:49:42 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240907/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240907/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 bool Check(int n) {
-    for (int i = 2; i < n; ++i)
+    if (n == 1)
+        return false;
+    else
     {
-        if (n % i == 0)
-            return false;
+        for (int i = 2; i < n; ++i)
+        {
+            if (n % i == 0)
+                return false;
+        }
     }
 
     return true;
