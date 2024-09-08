4bc348d916f374d6436fec98931cfe7aa22228a9
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-08 10:16:39 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240908/그 계절, 그 날/that-season-that-day.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int Y, M, D;
+
+bool YoonYear() {
+    if (Y % 4 == 0)
+    {
+        if (Y % 100 == 0)
+        {
+            if (Y % 400 == 0)
+                return true;
+
+            return false;
+        }
+        return true;
+    }
+    else
+        return false;
+}
+
+// 윤년일 경우 날짜 체크
+bool isPossible() {
+    if (YoonYear())
+    {
+        if (M == 2 && D <= 29)
+            return true;
+        else
+            return false;
+        
+        if (M == 4 || M == 6 || M == 9 || M == 11)
+        {
+            if (D <= 30)
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            if (D <= 31)
+                return true;
+            else
+                return false;
+        }
+    }
+    else
+    {
+        if (M == 2 && D <= 28)
+            return true;
+        else
+            return false;
+        
+        if (M == 4 || M == 6 || M == 9 || M == 11)
+        {
+            if (D <= 30)
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            if (D <= 31)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
+// 날짜가 존재하면 계절을 출력, 없으면 -1 출력
+void Check() {
+    if (isPossible())
+    {
+        if (M >= 3 && M <= 5)
+            cout << "Spring";
+        else if (M >= 6 && M <= 8)
+            cout << "Summer";
+        else if (M >= 9 && M <= 11)
+            cout << "Fall";
+        else if (M == 12 || M == 1 || M == 2)
+            cout << "Winter";
+    }
+    else
+        cout << -1; 
+}
+
+int main() {
+    cin >> Y >> M >> D;
+
+    Check();
+        
+    return 0;
+}
