9a67aadff9e429ea18d66657f06efbf671d8b4eb
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-08 12:08:16 +0900

    Title: palindrome 여부 판단하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 240908/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -3,17 +3,29 @@
 
 using namespace std;
 
-bool Pal(string text) {
-    string temp = "";
-    for (int i = text.length() - 1; i >= 0; --i)
-    {
-        temp += text[i];
-    }
+// 1. 문자열을 반대로 돌린 배열을 만들어 기존 배열과 비교하는 방법
+// bool Pal(string text) {
+//     string temp = "";
+//     for (int i = text.length() - 1; i >= 0; --i)
+//     {
+//         temp += text[i];
+//     }
 
-    if (temp == text)
+//     if (temp == text)
+//         return true;
+//     else
+//         return false;
+// }
+
+// 2. 포인터로 문자열을 가져와서 제일 앞과 끝을 순차적으로 비교하는 방법
+bool Pal(string &text) {
+    for (int i = 0; i < text.length(); ++i)
+    {
+        if (text[i] != text[text.length() - i - 1])
+            return false;
+        
         return true;
-    else
-        return false;
+    }
 }
 
 int main() {
