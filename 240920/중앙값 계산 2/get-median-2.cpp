5124ef1337b016b54416b27aaf8fa18684d506bd
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-20 23:04:48 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/중앙값 계산 2/get-median-2.cpp
+++ 240920/중앙값 계산 2/get-median-2.cpp
@@ -4,21 +4,15 @@
 using namespace std;
 
 int MiddleNum(int n, int arr[]) {
+    
     int temp[n + 1];
-    // cout << "temp배열 출력 : ";
+
     for(int i = 0; i <= n; ++i) {
         temp[i] = arr[i];
-        // cout << temp[i] << " ";
     }
 
     sort(temp, temp + n + 1);
 
-    // // cout << endl << "sort이후 temp배열 출력 : ";
-    // for(int i = 0; i <= n; ++i) {
-    //     cout << temp[i] << " ";
-    // }
-
-    // cout << endl << "중앙값 출력 : ";
     return temp[n / 2];
 }
 
@@ -37,7 +31,6 @@ int main() {
         if(i % 2 == 0) {
             cout << MiddleNum(i, arr) << " ";
         }
-        // cout << endl;
     }
 
     return 0;
