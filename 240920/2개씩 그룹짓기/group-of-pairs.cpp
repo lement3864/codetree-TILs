1b329ee5f5a68769dd910edfc13f91d6e9f701e6
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-20 18:13:58 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240920/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int N, maxSum = 0;
+    cin >> N;
+
+    int arr[2 * N];
+    for(int i = 0; i < 2 * N; ++i)
+        cin >> arr[i];
+    
+    sort(arr, arr + 2 * N);
+
+    for(int i = 0; i < N; ++i) {
+        if(arr[i] + arr[2 * N - i] >= maxSum) {
+            maxSum = arr[i] + arr[2 * N - i];
+        }
+    }
+
+    cout << maxSum;
+
+    return 0;
+}
