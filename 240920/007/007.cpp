a2191c67a2ed25320d35566a26c65890e53825f9
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-20 23:34:24 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240920/007/007.cpp
+++ 240920/007/007.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
 using namespace std;
 
-class Agent {
-    public:
-        string code;
-        char point;
-        int time;
 
-        Agent(string code, char point, int time) {
-            this->code = code;
-            this->point = point;
-            this->time = time;
-        }
-};
+// class를 사용해서 풀이
+// class Agent {
+//     public:
+//         string code;
+//         char point;
+//         int time;
+
+//         Agent(string code, char point, int time) {
+//             this->code = code;
+//             this->point = point;
+//             this->time = time;
+//         }
+// };
+
+// int main() {
+//     string code;
+//     char point;
+//     int time;
+
+//     cin >> code >> point >> time;
+
+//     Agent agentEX = Agent(code, point, time);
+
+//     cout << "secret code : " << agentEX.code << endl;
+//     cout << "meeting point : " << agentEX.point << endl;
+//     cout << "time : " << agentEX.time << endl;
+
+//     return 0;
+// }
+
+// tuple을 사용해서 풀이
+
+tuple<string, char, int> agent;
 
 int main() {
     string code;
@@ -23,11 +46,14 @@ int main() {
 
     cin >> code >> point >> time;
 
-    Agent agentEX = Agent(code, point, time);
+    agent = make_tuple(code, point, time);
 
-    cout << "secret code : " << agentEX.code << endl;
-    cout << "meeting point : " << agentEX.point << endl;
-    cout << "time : " << agentEX.time << endl;
+    string result_code;
+    char result_point;
+    int result_time;
+    tie(result_code, result_point, result_time) = agent;
 
-    return 0;
+    cout << "secret code : " << result_code << endl;
+    cout << "meeting point : " << result_point << endl;
+    cout << "time : " << result_time << endl;
 }
