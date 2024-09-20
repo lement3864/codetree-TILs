6cc5553cb6e3fc855fa93d3df64f4ecf9e5ed2b1
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-20 17:44:24 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240920/단어 정렬/sorting-words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    string arr[n];
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for(int i = 0; i < n; ++i) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
