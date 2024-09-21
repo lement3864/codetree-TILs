18c9a7e85d7d96f6b2508ba44635025607dbfd42
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-09-21 22:17:44 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/Next Level/next-level.cpp
+++ 240921/Next Level/next-level.cpp
@@ -1,18 +1,48 @@
+// class를 사용해서 풀이 =============================================
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// class Info {
+//     public:
+//         string id;
+//         int level;
+
+//         Info(string id = "codetree", int level = 10) {
+//             this->id = id;
+//             this->level = level;
+//         }
+// };
+
+// int main() {
+//     string text;
+//     int num;
+
+//     cin >> text >> num;
+
+//     Info user1 = Info();
+
+//     cout << "user " << user1.id << " lv " << user1.level << endl;    
+
+//     user1.id = text;
+//     user1.level = num;
+
+//     cout << "user " << user1.id << " lv " << user1.level;    
+
+//     return 0;
+// }
+
+// =======================================================================
+
+// tuple을 사용해서 풀이
+
 #include <iostream>
 #include <string>
+#include <tuple>
 
 using namespace std;
-
-class Info {
-    public:
-        string id;
-        int level;
-
-        Info(string id = "codetree", int level = 10) {
-            this->id = id;
-            this->level = level;
-        }
-};
 
 int main() {
     string text;
@@ -20,14 +50,20 @@ int main() {
 
     cin >> text >> num;
 
-    Info user1 = Info();
+    tuple<string, int> user = make_tuple("codetree", 10);
 
-    cout << "user " << user1.id << " lv " << user1.level << endl;    
+    string id;
+    int level;
 
-    user1.id = text;
-    user1.level = num;
+    tie(id, level) = user;
 
-    cout << "user " << user1.id << " lv " << user1.level;    
+    cout << "user " << id << " lv " << level << endl;
+
+    user = make_tuple(text, num);
+
+    tie(id, level) = user;
+
+    cout << "user " << id << " lv " << level;
 
     return 0;
 }
