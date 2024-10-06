8fbd42f949413058f9f97a1fd90108e6cc47b8e7
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-10-06 11:37:46 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int day = 11, h = 11, m = 11;
+    int cnt_time = 0;
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a < 11 || b < 11 || c < 11){
+        cout << -1;
+    }
+    else {
+        while (true) {
+            if (day == a && h == b && m == c)
+                break;
+            
+            cnt_time++;
+            m++;
+
+            if (m > 60) {
+                h++;
+                m = 1;
+            }
+            if (h > 24) {
+                day++;
+                h = 1;
+            }
+        }
+        cout << cnt_time;
+    }
+
+
+    return 0;
+}
