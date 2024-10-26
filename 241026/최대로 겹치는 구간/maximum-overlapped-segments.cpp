c543d93b1947af6ab83277cc9d3419fde36d05d3
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-10-26 10:25:11 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 241026/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     
-    int n, result = 0;
+    int n;
     int arr[201] = {};
 
     cin >> n;
@@ -12,12 +12,15 @@ int main() {
         int a, b;
         cin >> a >> b;
 
-        for (int j = a + 100; j <= b + 100; ++j) {
+        for (int j = a + 100; j < b + 100; ++j) {
             arr[j] += 1;
         }
     }
 
-    for (int i = 0; i <= 201; ++i) {
+    // 최댓값 구하기
+    int result = 0;
+
+    for (int i = 0; i < 201; ++i) {
         if (arr[i] >= result)
             result = arr[i];
     }
