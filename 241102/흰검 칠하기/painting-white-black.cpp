606a7a86f5f515a26ead9fb7bbb348d7f36722f3
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-11-02 10:01:42 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241102/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,144 @@
+// #include <iostream>
+
+// #define MAX_N 1000
+// #define MAX_R 200000
+// #define OFFSET 100000
+
+// using namespace std;
+
+// int n;
+// int x1[MAX_N];
+// int x2[MAX_N];
+// int checked[MAX_R + 1][2];
+
+// void check_W (int i) {
+//     for (int j = x1[i]; j < x2[i]; ++j) {
+//         checked[j][0] = 1;
+//         checked[j][1] += 1;
+//     }
+// }
+
+// void check_B (int i) {
+//     for (int j = x1[i]; j < x2[i]; ++j) {
+//         checked[j][0] = 2;
+//         checked[j][2] += 1;
+//     }
+// }
+
+// int main() {
+//     int cur = 0;
+//     int white = 0;
+//     int black = 0;
+//     int gray = 0;
+
+//     cin >> n;
+    
+//     for (int i = 0; i < n; ++i) {
+//         int distance;
+//         char direction;
+//         cin >> distance >> direction;
+
+//         if (direction == 'L') {
+//             x1[i] = cur - distance + OFFSET;
+//             x2[i] = cur + OFFSET;
+//             cur -= distance;
+//             check_W(i);
+//         }
+//         else {
+//             x1[i] = cur + OFFSET;
+//             x2[i] = cur + distance + OFFSET;
+//             cur += distance;
+//             check_B(i);
+//         }
+//     }
+
+//     for (int i = 0; i <= MAX_R; ++i) {
+//         // 회색인 경우
+//         if (checked[i][1] >= 2 && checked[i][2] >= 2) {
+//             gray++;
+//         }
+//         else {
+//             if (checked[i][0] == 1) {
+//                 white++;
+//             }
+//             else if (checked[i][0] == 2) {
+//                 black++;
+//             }
+//         } 
+//     }
+
+//     cout << white << " " << black << " " << gray;
+
+//     return 0;
+// }
+
+
+#include <iostream>
+
+#define MAX_N 1000
+#define MAX_R 200000
+#define OFFSET 100000
+
+using namespace std;
+
+int n;
+int x1[MAX_N];
+int x2[MAX_N];
+int white_count[MAX_R + 1] = {0};
+int black_count[MAX_R + 1] = {0};
+int last_color[MAX_R + 1] = {0}; // 0: 없음, 1: 흰색, 2: 검은색
+
+void check_W(int i) {
+    for (int j = x1[i]; j < x2[i]; ++j) {
+        white_count[j]++;
+        last_color[j] = 1; // 마지막으로 흰색으로 칠해짐
+    }
+}
+
+void check_B(int i) {
+    for (int j = x1[i]; j < x2[i]; ++j) {
+        black_count[j]++;
+        last_color[j] = 2; // 마지막으로 검은색으로 칠해짐
+    }
+}
+
+int main() {
+    int cur = 0;
+    int white = 0;
+    int black = 0;
+    int gray = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int distance;
+        char direction;
+        cin >> distance >> direction;
+
+        if (direction == 'L') {
+            x1[i] = cur - distance + OFFSET;
+            x2[i] = cur + OFFSET;
+            cur -= distance;
+            check_W(i);
+        } else {
+            x1[i] = cur + OFFSET;
+            x2[i] = cur + distance + OFFSET;
+            cur += distance;
+            check_B(i);
+        }
+    }
+
+    for (int i = 0; i <= MAX_R; ++i) {
+        if (white_count[i] >= 2 && black_count[i] >= 2) {
+            gray++;
+        } else if (last_color[i] == 1) {
+            white++;
+        } else if (last_color[i] == 2) {
+            black++;
+        }
+    }
+
+    cout << white << " " << black << " " << gray << endl;
+
+    return 0;
+}
