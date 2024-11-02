92d0dfc7c914cdcdf4c62f29fec3589a7c4608d2
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-11-02 10:21:59 +0900

    Title: 흰검 칠하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241102/흰검 칠하기/painting-white-black.cpp
+++ 241102/흰검 칠하기/painting-white-black.cpp
@@ -81,19 +81,19 @@
 using namespace std;
 
 int n;
-int checked[MAX_R + 1][2]; // [0]: 마지막으로 칠해진 색 (1: 흰색, 2: 검은색), [1]: 칠해진 총 횟수
+int checked[MAX_R + 1][3]; // [0]: 마지막으로 칠해진 색 (1: 흰색, 2: 검은색), [1]: 흰색 칠해진 횟수, [2]: 검은색 칠해진 횟수
 
 void check_W(int start, int end) {
     for (int i = start; i < end; ++i) {
         checked[i][0] = 1; // 마지막으로 흰색으로 칠해짐
-        checked[i][1]++;   // 칠해진 횟수 증가
+        checked[i][1]++;   // 흰색으로 칠해진 횟수 증가
     }
 }
 
 void check_B(int start, int end) {
     for (int i = start; i < end; ++i) {
         checked[i][0] = 2; // 마지막으로 검은색으로 칠해짐
-        checked[i][1]++;   // 칠해진 횟수 증가
+        checked[i][2]++;   // 검은색으로 칠해진 횟수 증가
     }
 }
 
@@ -124,12 +124,14 @@ int main() {
     }
 
     for (int i = 0; i <= MAX_R; ++i) {
-        // 회색인 경우: 최소 흰색과 검은색 각각 두 번 이상 칠해진 경우
-        if (checked[i][1] >= 4) { // 두 색 모두 두 번 이상 칠해진 경우 회색
+        // 회색인 경우: 흰색과 검은색으로 각각 두 번 이상 칠해진 경우
+        if (checked[i][1] >= 2 && checked[i][2] >= 2) {
             gray++;
-        } else if (checked[i][0] == 1) {
+        } else if (checked[i][0] == 1 && checked[i][2] < 2) {
+            // 마지막으로 흰색으로 칠해졌고, 검은색이 두 번 이상 칠해지지 않은 경우
             white++;
-        } else if (checked[i][0] == 2) {
+        } else if (checked[i][0] == 2 && checked[i][1] < 2) {
+            // 마지막으로 검은색으로 칠해졌고, 흰색이 두 번 이상 칠해지지 않은 경우
             black++;
         }
     }
