4b672774f20a4f51fd8cc1e50790886a388dc06a
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-11-29 20:46:04 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241129/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,43 @@
+// 1. 직사각형이 있는 위치 모두 1로 전환
+// 2. 1로 전환된 갯수 출력
+
+#include <iostream>
+using namespace std;
+
+int n;                                      // 직사각형 갯수
+int count = 0;                              // 직사각형 넓이
+int arr[201][201] = {0};                     // 좌표 평면
+
+int main() {
+    
+    cin >> n;
+
+
+    // 직사각형 위치 받아서 배열에 체크
+    for (int i = 0; i < n; ++i) {
+        
+        int x1, x2, y1, y2;                 // 직사각형의 양 끝 좌표
+
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        // 배열에 직사각형이 있는 곳에 1로 체크
+        for (int j = x1 + 100; j < x2 + 100; ++j) {
+            for (int k = y1 + 100; k < y2 + 100; ++k) {
+                arr[j][k] = 1;
+            }
+        }
+    }
+
+    // 배열에서 1로 체크되어 있는 갯수 출력
+    for (int i = 0; i < 201; ++i) {
+        for (int j = 0; j < 201; ++j) {
+            if (arr[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
