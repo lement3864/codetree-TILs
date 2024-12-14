8096b4b21071684b561ae92df9ad60be6b20dce8
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-12-14 10:15:45 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241214/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -0,0 +1,38 @@
+// 1. 이전 숫자보다 현재 숫자가 크고, t보다 크면 카운트
+// 2. 조건에서 벗어나면 리셋
+
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n; 
+int t;
+int arr[MAX_N];
+
+int main() {
+    
+    cin >> n >> t;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int result = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (i >= 1 && arr[i - 1] > t && arr[i] > arr[i - 1])
+            cnt++;
+        else
+            cnt = 1;
+        
+        result = max(result, cnt);
+    }
+    
+    cout << result;
+
+    return 0;
+}
