61019a0b999df247956755f20aad964dc98ad71d
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-12-14 19:23:42 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241214/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 241214/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -11,8 +11,8 @@ int arr[MAX_N];
 
 int main() {
 
-    int result = 0; // 최대 길이 저장
-    int cnt = 0;    // 현재 연속 부분 수열 길이
+    int result = 0;
+    int cnt = 0;
 
     cin >> n >> t;
 
@@ -22,14 +22,14 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         if (arr[i] > t) {
-            cnt++; // t보다 크면 현재 길이 증가
-            result = max(result, cnt); // 최대 길이 갱신
+            cnt++;
+            result = max(result, cnt);
         } else {
-            cnt = 0; // 조건에 맞지 않으면 초기화
+            cnt = 0;
         }
     }
 
-    cout << result << endl; // 결과 출력
+    cout << result;
 
     return 0;
 }
