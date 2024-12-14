94c271c077b35d255982157e1968331ede3dc2a8
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-12-14 10:16:28 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241214/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 241214/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -24,7 +24,7 @@ int main() {
     int cnt = 0;
 
     for (int i = 0; i < n; ++i) {
-        if (i >= 1 && arr[i - 1] > t && arr[i] > arr[i - 1])
+        if (i >= 1 && arr[i - 1] > t && arr[i] > t && arr[i] > arr[i - 1])
             cnt++;
         else
             cnt = 1;
