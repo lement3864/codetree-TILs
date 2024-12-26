0c095eb6c533ce1eff7dd34a76af3915cd3e9ff7
lement3864 <139476101+lement3864@users.noreply.github.com>
2024-12-26 10:51:19 +0900

    Title: 1이 3개 이상 있는 위치, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 241226/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 241226/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -8,7 +8,7 @@ int arr[101][101];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
-bool InRange(int x, int y, int n) {
+bool InRange(int x, int y) {
     return (0 <= x && x <= n && 0 <= y && y <= n);
 }
 
@@ -24,13 +24,13 @@ int main() {
 
     for (int i = 0; i < n; ++ i) {
         for (int j = 0; j < n; ++j) {
-            cnt = 0;
+            int cnt = 0;
 
             for (int k = 0; k < 4; ++k) {
-                nx = i + dx[k];
-                ny = j + dy[k];
+                int nx = i + dx[k];
+                int ny = j + dy[k];
                 
-                if (InRange(nx, ny, n) && arr[nx][ny] == 1)
+                if (InRange(nx, ny) && arr[nx][ny] == 1)
                     cnt++;
             }
 
